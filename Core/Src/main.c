3b4215d5d89ec1957f9b99cd3fc5a7b222016544
20521435 <20521435@gm.uit.edu.vn>
2022-12-28 11:49:34 +0700

    mini-project

---
+++ Core/Src/main.c
@@ -0,0 +1,532 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os.h"
+#include "usb_device.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stm32f429i_discovery_gyroscope.h"
+#include "stm32f429i_discovery_lcd.h"
+#include "stm32f429i_discovery_sdram.h"
+#include "usbd_cdc_if.h"
+#include <stdio.h>
+#include "string.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi5;
+
+osThreadId Task01Handle;
+osThreadId Task02Handle;
+osThreadId Task03Handle;
+osThreadId Task04Handle;
+osMessageQId myQueueHandle;
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_SPI5_Init(void);
+void StartTask01(void const * argument);
+void StartTask02(void const * argument);
+void StartTask03(void const * argument);
+void StartTask04(void const * argument);
+
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+typedef struct {
+	int16_t x;
+	int16_t y;
+	int16_t z;
+} Gyro_Data;
+float Ange[3];
+char buff[40];
+char score[40];
+uint16_t R;
+uint16_t X;
+uint16_t Y;
+uint8_t p = 0;
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_SPI5_Init();
+  /* USER CODE BEGIN 2 */
+  BSP_LCD_Init();
+  	BSP_SDRAM_Init();
+  	BSP_LCD_LayerDefaultInit(1, SDRAM_DEVICE_ADDR);
+  	BSP_LCD_SelectLayer(1);//select on which layer we write
+  	BSP_LCD_DisplayOn();//turn on LCD
+  	BSP_LCD_Clear(LCD_COLOR_BLUE);//clear the LCD on blue s
+  	BSP_LCD_SetBackColor(LCD_COLOR_BLUE);//set text background color
+  	BSP_LCD_SetTextColor(LCD_COLOR_WHITE);//set text color
+  	BSP_LCD_SetFont(&Font20);
+  	BSP_LCD_GetFont();
+  	BSP_LCD_FillCircle(120, 160, 10);
+  	BSP_LCD_DisplayStringAtLine(1, (uint8_t*) "Scored: 0");
+  /* USER CODE END 2 */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* Create the queue(s) */
+  /* definition and creation of myQueue */
+  osMessageQDef(myQueue, 32, int16_t);
+  myQueueHandle = osMessageCreate(osMessageQ(myQueue), NULL);
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* definition and creation of Task01 */
+  osThreadDef(Task01, StartTask01, osPriorityNormal, 0, 512);
+  Task01Handle = osThreadCreate(osThread(Task01), NULL);
+
+  /* definition and creation of Task02 */
+  osThreadDef(Task02, StartTask02, osPriorityAboveNormal, 0, 512);
+  Task02Handle = osThreadCreate(osThread(Task02), NULL);
+
+  /* definition and creation of Task03 */
+  osThreadDef(Task03, StartTask03, osPriorityNormal, 0, 512);
+  Task03Handle = osThreadCreate(osThread(Task03), NULL);
+
+  /* definition and creation of Task04 */
+  osThreadDef(Task04, StartTask04, osPriorityNormal, 0, 512);
+  Task04Handle = osThreadCreate(osThread(Task04), NULL);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief SPI5 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI5_Init(void)
+{
+
+  /* USER CODE BEGIN SPI5_Init 0 */
+
+  /* USER CODE END SPI5_Init 0 */
+
+  /* USER CODE BEGIN SPI5_Init 1 */
+
+  /* USER CODE END SPI5_Init 1 */
+  /* SPI5 parameter configuration*/
+  hspi5.Instance = SPI5;
+  hspi5.Init.Mode = SPI_MODE_MASTER;
+  hspi5.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi5.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi5.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi5.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi5.Init.NSS = SPI_NSS_SOFT;
+  hspi5.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi5.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi5.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi5.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi5.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI5_Init 2 */
+
+  /* USER CODE END SPI5_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13|GPIO_PIN_14, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PG13 PG14 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_14;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_StartTask01 */
+/**
+  * @brief  Function implementing the Task01 thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartTask01 */
+void StartTask01(void const * argument)
+{
+  /* init code for USB_DEVICE */
+  MX_USB_DEVICE_Init();
+  osSignalWait(0x1, osWaitForever);
+  /* USER CODE BEGIN 5 */
+  /* Infinite loop */
+  for(;;)
+  {
+	  if(R == 15) {
+		  R = 0;
+	  }
+	  sprintf(buff, "Ball High: %dcm", R);
+	  CDC_Transmit_HS((uint8_t*) buff, strlen(buff));
+    osDelay(1);
+  }
+  /* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_StartTask02 */
+/**
+* @brief Function implementing the Task02 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask02 */
+void StartTask02(void const * argument)
+{
+  /* USER CODE BEGIN StartTask02 */
+	BSP_GYRO_Init();
+	float Data[3];
+  /* Infinite loop */
+  for(;;)
+  {
+	BSP_GYRO_GetXYZ(Data);
+	for (int i = 0; i < 3; i++) {
+		Data[i] = Data[i] / 1000;
+		if ((Data[i] < 1 && Data[i] > 0) || (Data[i] > -1 && Data[i] < 0)) {
+			Ange[i] = 0;
+		}
+		 else {
+			 Ange[i] += Data[i];
+		 }
+	}
+	osSignalSet(Task03Handle, 0x1);
+
+    osDelay(1);
+  }
+  /* USER CODE END StartTask02 */
+}
+
+/* USER CODE BEGIN Header_StartTask03 */
+/**
+* @brief Function implementing the Task03 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask03 */
+void StartTask03(void const * argument)
+{
+  /* USER CODE BEGIN StartTask03 */
+
+	osSignalWait(0x1, osWaitForever);
+	float temp;
+  /* Infinite loop */
+  for(;;)
+  {
+	if (Ange[0] < -1) {
+		while (1) {
+			if (Ange[0] > 1) {
+				if (Ange[0] < 70) {
+					for (float i = 16; i < 41; i = i + 0.01) {
+						R = i;
+						BSP_LCD_FillCircle(120, 160, i);
+						osSignalSet(Task01Handle, 0x1);
+					}
+					for (float j = R; j > 15; j = j - 0.03) {
+						R = j;
+						temp = (float) j - R;
+						if (temp < 0.03) {
+							BSP_LCD_Clear(LCD_COLOR_BLUE);
+						}
+						if (j > 15 && j < 15.50) {
+							osSignalSet(Task04Handle, 0x1);
+						}
+						sprintf(score, "Scored: %d", p);
+						BSP_LCD_DisplayStringAtLine(1, (uint8_t*) score);
+						BSP_LCD_FillCircle(120, 160, j);
+						osSignalSet(Task01Handle, 0x1);
+
+					}
+				}
+				else {
+					if (Ange[0] > 69 && Ange[0] < 80) {
+						for (float i = 16; i < 61; i = i + 0.01) {
+							R = i;
+							BSP_LCD_FillCircle(120, 160, i);
+							osSignalSet(Task01Handle, 0x1);
+						}
+						for (float j = R; j > 15; j = j - 0.03) {
+							R = j;
+							temp = (float) j - R;
+							if (temp < 0.03) {
+								BSP_LCD_Clear(LCD_COLOR_BLUE);
+							}
+							if (j > 15 && j < 15.50) {
+								osSignalSet(Task04Handle, 0x1);
+							}
+							sprintf(score, "Scored: %d", p);
+							BSP_LCD_DisplayStringAtLine(1, (uint8_t*) score);
+							BSP_LCD_FillCircle(120, 160, j);
+							osSignalSet(Task01Handle, 0x1);
+						}
+					}
+				}
+				if (Ange[0] < 0) {
+					osSignalSet(Task04Handle, 0x2);
+				}
+			}
+		}
+	}
+	else {
+		R = 0;
+		osSignalSet(Task01Handle, 0x1);
+	}
+    osDelay(1);
+  }
+  /* USER CODE END StartTask03 */
+}
+
+/* USER CODE BEGIN Header_StartTask04 */
+/**
+* @brief Function implementing the Task04 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask04 */
+void StartTask04(void const * argument)
+{
+  /* USER CODE BEGIN StartTask04 */
+  /* Infinite loop */
+  for(;;)
+  {
+	osSignalWait(0x1, osWaitForever);
+	if (Ange[0] > 1) {
+		p = p + 1;
+		HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13, 1);
+		osDelay(500);
+		HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13, 0);
+		osSignalWait(0x2, osWaitForever);
+	}
+	else {
+			p = 0;
+			while (1) {
+				osThreadSuspend(Task02Handle);
+				osThreadSuspend(Task03Handle);
+				HAL_GPIO_WritePin(GPIOG, GPIO_PIN_14, 1);
+				BSP_LCD_Clear(LCD_COLOR_RED);
+				BSP_LCD_SetBackColor(LCD_COLOR_RED);
+				BSP_LCD_DisplayStringAtLine(5, (uint8_t*) "    GAME OVER! ");
+				BSP_LCD_DisplayStringAtLine(6, (uint8_t*) "   PLEASE ENTER ");
+				BSP_LCD_DisplayStringAtLine(7, (uint8_t*) " RESET TO RESUME! ");
+				osDelay(osWaitForever);
+			}
+	}
+    osDelay(1);
+  }
+  /* USER CODE END StartTask04 */
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM6 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM6) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
